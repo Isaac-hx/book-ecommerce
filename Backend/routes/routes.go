e19912214a9dafe9034be32911057d3e7e9aa65d
rafly <raflynvd16@gmail.com>
2024-08-04 00:31:15 +0700

    feat(handler & route) register handler & routing for register

---
+++ Backend/routes/routes.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"Backend/handlers"
+)
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+	r := gin.Default()
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+	corsConfig.AllowHeaders = []string{"Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization"}
+
+	corsConfig.AllowCredentials = true
+	corsConfig.AddAllowMethods("OPTIONS")
+
+	r.Use(cors.New(corsConfig))
+
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
+	r.POST("/register", handlers.Register)
+
+	return r
+}
