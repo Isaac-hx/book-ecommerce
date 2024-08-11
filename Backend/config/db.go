0323bb7fa4daf7644b1ef09a7eae29b1a73f26d4
Isaac-hx <saydimas78@gmail.com>
2024-08-11 22:49:11 +0700

    clear godotenv.load

--- Backend/config/db.go
+++ Backend/config/db.go
@@ -3,12 +3,9 @@ package config
 import (
 	// "fmt"
 	"Backend/models"
-	"Backend/utils"
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 
 	"gorm.io/gorm"
@@ -20,44 +17,58 @@ import (
 func ConnectDB() *gorm.DB{
 	var db *gorm.DB
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file",err)
-	}
-	//mengambil nilai env
-	environment:=utils.Getenv("ENVIRONMENT","local")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file",err)
+	// }
+	// //mengambil nilai env
+	// environment:=utils.Getenv("ENVIRONMENT","local")
 
-	if environment == "local"{
-		//username:= utils.Getenv("USERNAME","root")
-		//password:= ""
-		database := utils.Getenv("DATABASE_NAME", "ecommerce_books")
-		host := utils.Getenv("HOST", "127.0.0.1")
-		port := utils.Getenv("PORT", "3306")
+	// if environment == "local"{
+	// 	//username:= utils.Getenv("USERNAME","root")
+	// 	//password:= ""
+	// 	database := utils.Getenv("DATABASE_NAME", "ecommerce_books")
+	// 	host := utils.Getenv("HOST", "127.0.0.1")
+	// 	port := utils.Getenv("PORT", "3306")
 
-		dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", "root", "", host, port, database)
-		dbGorm, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", "root", "", host, port, database)
+	// 	dbGorm, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-		if err != nil {
-			fmt.Println(dsn)
-			panic(err.Error())
-		}
-		db = dbGorm
+	// 	if err != nil {
+	// 		fmt.Println(dsn)
+	// 		panic(err.Error())
+	// 	}
+	// 	db = dbGorm
 
-	} else {
-		username := os.Getenv("DB_USERNAME")
-		password := os.Getenv("DB_PASSWORD")
-		host := os.Getenv("DB_HOST")
-		port := os.Getenv("DB_PORT")
-		database := os.Getenv("DB_NAME")
+	// } else {
+	// 	username := os.Getenv("DB_USERNAME")
+	// 	password := os.Getenv("DB_PASSWORD")
+	// 	host := os.Getenv("DB_HOST")
+	// 	port := os.Getenv("DB_PORT")
+	// 	database := os.Getenv("DB_NAME")
+	// 	// production
+	// 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	// 	dbGorm, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+
+	// 	db = dbGorm
+	// }
+
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_NAME")
 		// production
-		dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
-		dbGorm, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err != nil {
-			panic(err.Error())
-		}
-
-		db = dbGorm
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	dbGorm, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
 	}
+
+	db = dbGorm
 
 	//auto migration table saat terjadi perubahan atau penambahan models
 	// ini gw pindahin biar ngubah sekali aja
