932123ed4d12967af297305efe58d98ba05ee0f1
Ilmi Nur Azis Aqli Mubarok <ilmimubarok21@gmail.com>
2024-08-12 19:20:26 +0700

    fix: auto redirect to admin dashboard if admin

--- Frontend/src/components/layouts/AppShell/index.tsx
+++ Frontend/src/components/layouts/AppShell/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 import { TanstackQueryProvider, ThemeProvider } from "@/components/providers";
 import { Toaster } from "@/components/ui/sonner";
@@ -10,7 +11,16 @@ import { Footer } from "../Footer";
 import { Navbar } from "../Navbar";
 
 export const AppShell = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const pathname = usePathname();
   const { role } = useAuthStore((state) => state);
+
+  useEffect(() => {
+    if (!pathname.startsWith("/admin") && role === "admin") {
+      router.replace("/admin/home");
+    }
+  }, [router, pathname, role]);
+
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <TanstackQueryProvider>
